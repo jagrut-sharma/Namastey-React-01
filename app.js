ec27a9805b9735165f539d4c1e85bff41c64f07d
Jagrut Sharma <101259156+jagrut-sharma@users.noreply.github.com>
2023-08-30 20:47:27 +0530

    3

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namastey React 🚀"
-);
-console.log(heading); // {$$typeof: Symbol(react.element), type: 'h1', key: null, ref: null, props: {…}, …}
+// JSX Element:
+const Title = () => <h1 id="heading">Namastey React using JSX 🚀</h1>;
 
-// JSX: HTML-like or XML-like syntax
-const jsxHeading = <h1 id="heading">Namastey React using JSX 🚀</h1>;
-console.log(jsxHeading);
+const sampleHeading = <h2>Sample Heading</h2>;
+
+const number = 100;
+
+// Functional Component:
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1 className="heading">Namaste React Functional Component</h1>
+      {sampleHeading}
+      <h3>{number + 100}</h3>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
